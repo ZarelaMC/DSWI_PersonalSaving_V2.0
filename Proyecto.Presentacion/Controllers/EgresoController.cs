445d7b3370b9fabb924a65b6f2b0aa0c9e422dc9
FatimaUrpi <fc200509fatima.chavez09@gmail.com>
2024-06-27 23:43:48 -0500

    EgresoCrud

--- Proyecto.Presentacion/Controllers/EgresoController.cs
+++ Proyecto.Presentacion/Controllers/EgresoController.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FRONT_web_personal_saving.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 using Proyecto.Presentacion.Models;
+using System.Text;
 
 namespace Proyecto.Presentacion.Controllers
 {
@@ -8,48 +13,132 @@ namespace Proyecto.Presentacion.Controllers
     {
 
 
-            //Definir la cadena de conexion 
-            public readonly IConfiguration Configuration;
+        //Cadena conexión hacia el servicio
+        Uri baseAddress = new Uri("https://localhost:7035/api");
 
-            public EgresoController(IConfiguration configuration)
+        private readonly HttpClient _httpClient;
+
+        public EgresoController()
+        {
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
+
+            _httpClient = new HttpClient(handler);
+            _httpClient.BaseAddress = baseAddress;
+        }
+
+
+
+        //Listado Egresos
+        [HttpGet]
+        public IActionResult listarEgresos()
+        {
+            List<EgresoModel> aEgresos = new List<EgresoModel>();
+            HttpResponseMessage response =
+                _httpClient.GetAsync(_httpClient.BaseAddress + "/Egresos/listadoEgresos").Result;
+            if (response.IsSuccessStatusCode)
             {
-                this.Configuration = configuration;
+                var data = response.Content.ReadAsStringAsync().Result;
+                aEgresos = JsonConvert.DeserializeObject<List<EgresoModel>>(data);
+                @ViewBag.Egresos = aEgresos;
+                @ViewBag.primero = aEgresos.FirstOrDefault();
+            }
+            return View(aEgresos);
+        }
+
+        //Registro Egresos
+        [HttpGet]
+        public IActionResult NuevoEgreso()
+        {
+            return View(new EgresoModelO());
+        }
+        [HttpPost]
+        public async Task<IActionResult> NuevoEgreso(EgresoModelO objE)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(new EgresoModelO());
+            }
+            var json = JsonConvert.SerializeObject(objE);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var responseC = await
+            _httpClient.PostAsync("api/Egresos/nuevoEgreso", content);
+            if (responseC.IsSuccessStatusCode)
+            {
+                ViewBag.mensaje = "Egreso registrado correctamente..!!!";
+            }
+            //ViewBag.codigoI = json;
+            return View(objE);
+        }
+
+        //Actualización de Egresos
+        [HttpGet]
+        public async Task<IActionResult> ModificarEgreso(int id)
+        {
+            var response = await
+            _httpClient.GetAsync(_httpClient.BaseAddress + "/Egresos/buscarEgreso/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var objE = JsonConvert.DeserializeObject<EgresoModelO>(content);
+                return View(objE);
+            }
+            else
+            {
+                ViewBag.mensaje = "No hay Egreso!!!";
             }
 
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> ModificarEgreso(int id, EgresoModelO objE)
+        {
 
-            public List<Egreso> aEgreso()
+            var json = JsonConvert.SerializeObject(objE);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PutAsync("api/Egresos/modificaEgreso?id={ id }", content);
+            if (response.IsSuccessStatusCode)
             {
-                List<Egreso> aEgreso = new List<Egreso>();
-                SqlConnection cn = new SqlConnection(Configuration["ConnectionStrings:cn"]);
-                cn.Open();
-                SqlCommand cmd = new SqlCommand("SP_LISTAR_EGRESO", cn);
-                cmd.CommandType = System.Data.CommandType.StoredProcedure;
-                SqlDataReader dr = cmd.ExecuteReader();
-                while (dr.Read())
+                ViewBag.mensaje = "Egreso actualizado correctamente..!!!";
+            }
+            return View(objE);
+        }
+
+
+        //Eliminación 
+        [HttpDelete]
+        public async Task<IActionResult> EliminarEgreso(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"api/Egresos/eliminaEgreso/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseData = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<dynamic>(responseData);
+
+                if (result.success == true)
                 {
-                    aEgreso.Add(new Egreso()
-                    {
-                        id = int.Parse(dr[0].ToString()),
-                        id_usuario = int.Parse(dr[1].ToString()),
-                        id_transaccion = int.Parse(dr[2].ToString()),
-                        fecha = DateTime.Parse(dr[3].ToString()),
-                        monto = double.Parse(dr[4].ToString()),
-                        descripcion = dr[5].ToString()
-
-                    });
+                    ViewBag.mensaje = "Egreso eliminado correctamente..!!!";
+                    return RedirectToAction("listarEgresos"); // Redirigir al listado después de la eliminación
                 }
-                cn.Close();
-                return aEgreso;
+                else
+                {
+                    ViewBag.mensaje = "No se eliminó el egreso: " + result.message;
+                    return RedirectToAction("listarEgresos");
+                }
             }
-
-            public IActionResult ListaEgreso()
+            else
             {
-                return View(aEgreso());
+                ViewBag.mensaje = "Error en la comunicación con el servidor.";
+                return RedirectToAction("listarEgresos");
             }
+        }
 
-            public IActionResult Index()
+
+        public IActionResult Index()
         {
             return View();
         }
     }
 }
+
---
+++ Proyecto.Presentacion/Models/EgresoModelO.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+
+namespace FRONT_web_personal_saving.Models
+{
+    public class EgresoModelO
+    {
+
+        [DisplayName("CÓDIGO")]
+
+        public int id { get; set; }
+        [DisplayName("CÓDIGO USUARIO")]
+
+        public int id_usuario { get; set; }
+
+        [DisplayName("CÓDIGO TRANSACCIÓN")]
+
+        public int id_transaccion { get; set; }
+
+        [DisplayName("FECHA")]
+
+        public DateTime fecha { get; set; }
+
+        [DisplayName("MONTO")]
+        public double monto { get; set; }
+
+        [DisplayName("DESCRIPCIÓN")]
+        public string? descripcion { get; set; }
+
+        [DisplayName("ESTADO")]
+
+        public int estado { get; set; }
+    }
+}
